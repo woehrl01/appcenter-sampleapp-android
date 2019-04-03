771a6ef88dbc3074c0e8be2d0beacf3eddf9a52c
Lukas Wöhrl <lukas.woehrl@microsoft.com>
2019-04-03 15:59:23 +0200

    fix espresso reference

--- app/src/androidTest/java/com/example/appcenter/sampleapp_android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/appcenter/sampleapp_android/ExampleInstrumentedTest.java
@@ -4,10 +4,15 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import com.microsoft.appcenter.espresso.Factory;
+import com.microsoft.appcenter.espresso.ReportHelper;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -16,6 +21,10 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ReportHelper reportHelper = Factory.getReportHelper();
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
